eb239e23f320ce67f02d926bd33bc155aabd952f
Lucas Neves <lneves@pragmaticworks.com>
2020-12-25 22:37:35 -0300

    Updates

---
+++ precache-manifest.c2784da6d2f20305bc257247769084f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b54e9ac36af728a1e0128b5ee741a1",
+    "url": "/hp-game/index.html"
+  },
+  {
+    "revision": "e09c89f88860f8236a54",
+    "url": "/hp-game/static/js/2.02a33f33.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/hp-game/static/js/2.02a33f33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a95010984c3b1b4359cb",
+    "url": "/hp-game/static/js/main.f5028408.chunk.js"
+  },
+  {
+    "revision": "bd2092f89ece9a1a72b7",
+    "url": "/hp-game/static/js/runtime-main.149310f6.js"
+  },
+  {
+    "revision": "61b7c23bc3d58e4c4b7776285606a2b6",
+    "url": "/hp-game/static/media/hog-bg.61b7c23b.webp"
+  },
+  {
+    "revision": "767b51a5fb4895312f9073b33fa0cc79",
+    "url": "/hp-game/static/media/hp-title.767b51a5.png"
+  },
+  {
+    "revision": "ab4bd9ce895369bbcfb0201b9d7f54c2",
+    "url": "/hp-game/static/media/pnghat.ab4bd9ce.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.db3be1575e6dfe14a3e2a0c473486bc6.js"
+  "/hp-game/precache-manifest.c2784da6d2f20305bc257247769084f4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hp-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
